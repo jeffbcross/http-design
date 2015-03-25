62cb9645e815434e43156bb50c320f77d0e9f98a
Jeff Cross <middlefloor@gmail.com>
2015-03-24 21:06:49 -0700

    fix(prototype): add observers to BaseConnectionConfig

--- prototype/public/BaseConnectionConfig.ts
+++ prototype/public/BaseConnectionConfig.ts
@@ -23,6 +23,9 @@ export class BaseConnectionConfig implements IConnectionConfig {
     method: string;
     url: string;
     cold: boolean;
+    downloadObserver: Rx.Observer<Response>;
+    uploadObserver: Rx.Observer<any>
+    stateObserver: Rx.Observer<any>
 
     constructor(source?: IConnectionConfig) {
         this.method = (source && source.method) || Methods.GET;
